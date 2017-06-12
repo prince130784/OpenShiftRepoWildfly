1155ac19b5545a355a177eb4805fb40de73c9ae0
RJ <rj@rj.com>
2017-06-12 17:53:12 -0400

    some more changes

--- src/main/java/test/springboot/openshift/openshift_demo_springboot/OpenshiftDemoSpringbootApplication.java
+++ src/main/java/test/springboot/openshift/openshift_demo_springboot/OpenshiftDemoSpringbootApplication.java
@@ -2,8 +2,6 @@ package test.springboot.openshift.openshift_demo_springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class OpenshiftDemoSpringbootApplication {
@@ -14,11 +12,11 @@ public class OpenshiftDemoSpringbootApplication {
 	
 }
 
-@RestController
+/*@RestController
 class HomeController{
 	
 	@GetMapping("/")
 	public String response(){
 		return "Hello Ravinderjeet!! Welcome to your first Cloup App";
 	}
-}
+}*/
--- src/main/java/test/springboot/openshift/openshift_demo_springboot/controller/HomeController.java
+++ src/main/java/test/springboot/openshift/openshift_demo_springboot/controller/HomeController.java
@@ -1,5 +1,14 @@
 package test.springboot.openshift.openshift_demo_springboot.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
+	
+	@GetMapping("/")
+	public String response(){
+		return "Hello Ravinderjeet!! Welcome to your first Cloup App.............";
+	}
 
 }
